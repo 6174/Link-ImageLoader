3cd99ac97aef37f6c800474c549ee9f7a9c5006f
Link Yan <wsyanligang@126.com>
2013-05-09 17:18:24 +0800

    d

--- library/gen/cn/link/imageloader/Manifest.java
+++
@@ -1,5 +0,0 @@
-package cn.link.imageloader;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
--- library/gen/cn/link/imageloader/R.java
+++
@@ -1,5 +0,0 @@
-package cn.link.imageloader;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
